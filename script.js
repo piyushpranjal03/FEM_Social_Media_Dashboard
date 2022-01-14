1dc8843f33f3409e512488572ac5fad94b9f778d
Piyush <42115207+piyushpranjal03@users.noreply.github.com>
2022-01-15 00:05:33 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Subs & Followers Card Template Part
+{
+  /* <div class="card-wrapper facebook">
+<div class="card-item">
+ <div class="title">
+  <img src="images/icon-facebook.svg" alt="Facebook Icon"><span>@nathanf</span>
+ </div>
+ <p>1987</p>
+ <p>FOLLOWERS</p>
+ <div>
+  <img src="images/icon-up.svg" alt="Up Icon"><span class="count-up">12 Today</span>
+ </div>
+</div>
+</div> */
+}
+
+// Overview Cards Template Part
+{
+  /* <div class="card-item">
+<div class="overview">
+ <span>Page Views</span>
+ <img src="images/icon-facebook.svg" alt="Facebook Icon">
+</div>
+<div class="overview other-count">
+ <span>87</span>
+ <div>
+  <img src="images/icon-up.svg" alt="">
+  <span class="count-up">3%</span>
+ </div>
+</div>
+</div> */
+}
