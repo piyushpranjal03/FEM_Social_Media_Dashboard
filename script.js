c3cdbb78ef61ab5b4b0636fd2ac41e7f431627d8
Piyush <42115207+piyushpranjal03@users.noreply.github.com>
2022-01-16 00:20:49 +0530

    Added Feature - Storing User Theme Preference

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 const body = document.body;
 const toggleBtn = document.querySelector("input[type='checkbox']");
 
-if (toggleBtn.checked) {
-  body.classList.add("dark-theme");
-} else {
-  body.classList.add("light-theme");
-}
+// Checking if any key "theme" is present in localStorage
+// "theme" key will be there if user ever toggle b/w modes
+// If "theme" key is present, currentTheme will be init with "dark" or "light"
+// If "theme" key isn't present, currentTheme will be init with null
+let currentTheme = localStorage.getItem("theme");
 
-function darkModeToggle() {
+// If currentTheme is dark we will turn the checkbox checked
+// If currentTheme is light or null we will keep the checkbox unchecked
+currentTheme == "dark" ? (toggleBtn.checked = true) : (toggleBtn.checked = false);
+
+// If currentTheme is "dark" or "light", same will be set to body
+// If currentTheme is  null, "light" will be set to body
+currentTheme = currentTheme ? body.setAttribute("data-mode", currentTheme) : body.setAttribute("data-mode", "light");
+
+function darkMode() {
   if (toggleBtn.checked) {
-    body.classList.remove("light-theme");
-    body.classList.add("dark-theme");
+    // Setting the user chosen theme in browsers local storage
+    localStorage.setItem("theme", "dark");
+    body.setAttribute("data-mode", localStorage.getItem("theme"));
   } else {
-    body.classList.remove("dark-theme");
-    body.classList.add("light-theme");
+    localStorage.setItem("theme", "light");
+    body.setAttribute("data-mode", localStorage.getItem("theme"));
   }
 }
 
-toggleBtn.addEventListener("change", darkModeToggle);
+// Keeping an eye on the toggleBtn state, whenever changed function will be triggered
+toggleBtn.addEventListener("change", darkMode);
 
 //
 //
